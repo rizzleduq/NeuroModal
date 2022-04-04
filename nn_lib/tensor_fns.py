softmax fixed

nn_lib/tensor_fns.py
@@ -46,7 +46,7 @@ def clip(x: Tensor, lower: Tensor, upper: Tensor) -> Tensor:
     return clip_from_above
 
 
-def reduce(x: Tensor, axis: Union[int, Tuple[int, ...], None] = None, reduction: str = 'mean') -> Tensor:
+def reduce(x: Tensor, axis: Union[int, Tuple[int, ...], None] = None, reduction: str = 'mean', keepdims: bool = False) -> Tensor:
     """
     Apply reduction to a Tensor
     :param x: Tensor to be reduced
@@ -63,7 +63,7 @@ def reduce(x: Tensor, axis: Union[int, Tuple[int, ...], None] = None, reduction:
         axis = (axis,)
 
     # first reduce by summation
-    result = Tensor.apply_fn(SumReduce, x, axis=axis)
+    result = Tensor.apply_fn(SumReduce, x, axis=axis, keepdims=keepdims)
     if reduction == 'mean':
         # if reduction is 'mean' divide result by the total size of reduced axes
         denominator = np.prod(tuple(map(lambda i: shape[i], axis)))
@@ -72,5 +72,5 @@ def reduce(x: Tensor, axis: Union[int, Tuple[int, ...], None] = None, reduction:
 
 def softmax(x:Tensor) -> Tensor:
     numerator=exp(x)
-    denominator=reduce(numerator,axis=0,reduction='sum')
+    denominator=reduce(numerator,axis=1,keepdims=True,reduction='sum')
     return numerator/denominator